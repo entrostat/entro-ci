ccaba9907fc0c2e53430b46c62250a8c07f10f08
Kerren <kerren@entrostat.com>
2021-01-18 20:50:49 +0200

    fix(build): Removed the reuse of the alias key array

--- src/modules/shared/transforms/alias-keys.transform.ts
+++ src/modules/shared/transforms/alias-keys.transform.ts
@@ -1,24 +1,30 @@
 import { Transform } from 'class-transformer';
 
-export function AliasKeys(keys: string[] | string): PropertyDecorator {
+export function AliasKeys(aliasKeys: string[] | string): PropertyDecorator {
     return Transform((value, obj) => {
         // If we have a value at this key then there is no need to continue
         if (value) {
             return value;
         }
-        if (Array.isArray(keys)) {
-            for (const key of keys) {
-                if (obj[key] !== undefined) {
-                    return obj[key];
+        if (Array.isArray(aliasKeys)) {
+            for (const aliasKey of aliasKeys) {
+                if (obj[aliasKey] !== undefined) {
+                    return obj[aliasKey];
                 }
             }
             return value;
         } else {
-            return obj[keys];
+            return obj[aliasKeys];
         }
     }) as (target: Object, propertyKey: string | symbol) => void;
 }
 
-export function AliasKey(key: string): PropertyDecorator {
-    return AliasKeys([key]);
+export function AliasKey(aliasKey: string): PropertyDecorator {
+    return Transform((value, obj) => {
+        // If we have a value at this key then there is no need to continue
+        if (value) {
+            return value;
+        }
+        return obj[aliasKey];
+    }) as (target: Object, propertyKey: string | symbol) => void;
 }
