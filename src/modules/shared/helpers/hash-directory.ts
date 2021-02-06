27abfb8c252031870f5082001bd5c41673c996e3
Kerren <kerren@entrostat.com>
2021-02-06 21:55:44 +0200

    Cater for spaces in the directory

--- src/modules/shared/helpers/hash-directory.ts
+++ src/modules/shared/helpers/hash-directory.ts
@@ -4,7 +4,10 @@ export async function hashDirectory(directory: string): Promise<string> {
     // Check here for the command:
     // https://stackoverflow.com/questions/545387/linux-compute-a-single-hash-for-a-given-folder-contents
 
-    let hash = await executeCommand(`find ${directory} -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum`, false);
+    let hash = await executeCommand(
+        `find '${directory}' -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum`,
+        false,
+    );
     hash = hash.replace(/ +-/, '');
     hash = hash.replace(/\n/, '');
     return hash;
