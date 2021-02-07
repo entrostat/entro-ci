147387b24d684be72bf4bc73d2b1bcb867393737
Kerren <kerren@entrostat.com>
2021-02-07 17:43:10 +0200

    Echo all of the hashes and create a final hash

--- src/modules/shared/helpers/hash-directories.ts
+++ src/modules/shared/helpers/hash-directories.ts
@@ -1,14 +1,13 @@
 import { executeCommand } from './execute-command';
+import { hashDirectory } from './hash-directory';
 
-export async function hashDirectories(directory: string): Promise<string> {
-    // Check here for the command:
-    // https://stackoverflow.com/questions/545387/linux-compute-a-single-hash-for-a-given-folder-contents
+export async function hashDirectories(directories: string[]): Promise<string> {
+    const hashes = await Promise.all(directories.map(directory => hashDirectory(directory)));
+    const sortedHashes = hashes.sort();
 
-    let hash = await executeCommand(
-        `find ${directory.replace(/ /g, '\\ ')} -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum`,
-        false,
-    );
+    let hash = await executeCommand(`echo ${sortedHashes.map(hash => `"${hash}"`).join(' ')} | sha1sum`, false);
     hash = hash.replace(/ +-/, '');
     hash = hash.replace(/\n/, '');
+
     return hash;
 }
