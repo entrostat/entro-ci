a55a094eafb9b25999ab3f8dbc92699627628ff7
Kerren <kerren@entrostat.com>
2021-09-26 17:55:28 +0200

    feat(version): Added the ability to pack and update the readme during the versioning

--- src/modules/shared/transforms/alias-key.transform.ts
+++ src/modules/shared/transforms/alias-key.transform.ts
@@ -1,7 +1,10 @@
 import { Transform } from 'class-transformer';
 
 export function AliasKey(aliasKey: string): PropertyDecorator {
-    return Transform((value, obj) => {
+    // I'm ignoring this for now, it's not being used currently.
+    // TODO: Remove this when possible
+    // @ts-ignore
+    return Transform((value: any, obj: any) => {
         // If we have a value at this key then there is no need to continue
         if (value) {
             return value;
